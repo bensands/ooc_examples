5c7269b2addf583ef23395ad7ca495b907ef10ca
Benny <bfs43@cornell.edu>
2018-11-24 09:55:23 -0500

    example 01

--- 01/Set.c
+++ 01/Set.c
@@ -5,19 +5,22 @@
 #include "Set.h"
 #include "Object.h"
 
-const void * Set;
-const void * Object;
+const void *Set;
+const void *Object;
 
 #if ! defined MANY || MANY < 1
 #define	MANY	10
 #endif
 
+// heap[] is array, indices represent objects
+// and the elements at the indices represent sets.
 static int heap[MANY];
 
 void *new(const void *type, ...)
 {	
 	int *p;							/* & heap[1..] */
-
+	
+	// why are we starting at heap + 1 ?
 	for (p = heap + 1; p < heap + MANY; ++p)
 	{
 		if (! *p)
@@ -32,57 +35,68 @@ void *new(const void *type, ...)
 	return p;
 }
 
-void delete (void * _item)
-{	int * item = _item;
+void delete(void *_item)
+{	
+	int *item = _item;
 
 	if (item)
-	{	assert(item > heap && item < heap + MANY);
-		* item = 0;
+	{	
+		assert(item > heap && item < heap + MANY);
+		*item = 0;
 	}
 }
 
-void * add (void * _set, const void * _element)
-{	int * set = _set;
-	const int * element = _element;
+void *add(void *_set, const void *_element)
+{	
+	// local copies of void pointers w/ appropriate types
+	int *set = _set;
+	const int *element = _element;
 
 	assert(set > heap && set < heap + MANY);
-	assert(* set == MANY);
+	assert(*set == MANY);
 	assert(element > heap && element < heap + MANY);
 
-	if (* element == MANY)
-		* (int *) element = set - heap;
+	if (*element == MANY)
+		*(int *)element = set - heap;
 	else
-		assert(* element == set - heap);
+		assert(*element == set - heap);
 
-	return (void *) element;
+	return (void *)element;
 }
 
-void * find (const void * _set, const void * _element)
-{	const int * set = _set;
-	const int * element = _element;
+void *find(const void *_set, const void *_element)
+{
+	// local copies of void pointers w/ appropriate types	
+	const int *set = _set;
+	const int *element = _element;
 
 	assert(set > heap && set < heap + MANY);
-	assert(* set == MANY);
+	assert(*set == MANY);
 	assert(element > heap && element < heap + MANY);
-	assert(* element);
+	assert(*element);
 
-	return * element == set - heap ? (void *) element : 0;
+	return *element == set - heap ? (void *) element : 0;
 }
 
-int contains (const void * _set, const void * _element)
+int contains(const void *_set, const void *_element)
 {
 	return find(_set, _element) != 0;
 }
 
-void * drop (void * _set, const void * _element)
-{	int * element = find(_set, _element);
+void *drop(void *_set, const void *_element)
+{	
+	int *element = find(_set, _element);
 
 	if (element)
-		* element = MANY;
+	{
+		*element = MANY;
+	}
+
 	return element;
 }
 
-int differ (const void * a, const void * b)
+int differ(const void *a, const void *b)
 {
 	return a != b;
 }
+
